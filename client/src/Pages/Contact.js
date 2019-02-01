abd23deefed92ead92160c20fdae1169f161a6c4
KHollobaugh <khollobaugh@gmail.com>
2019-02-01 16:04:52 -0500

    reactstrap form

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 
 const Contact = () => {
@@ -7,6 +7,25 @@ const Contact = () => {
         <div className="contact-page">
 
         <h1>Contact</h1>
+        <Form>
+
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="name" name="name" id="exampleName" placeholder="your name" />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="name">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="your email" />
+        </FormGroup>
+
+
+        <FormGroup>
+          <Label for="exampleMessage">Message</Label>
+          <Input type="message" name="message" id="message" />
+        </FormGroup>
+
+      </Form>
         
         </div>
     )
