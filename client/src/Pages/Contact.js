307cd18a65179ada6a94b14b3b84cd3713c9b41d
KHollobaugh <khollobaugh@gmail.com>
2019-02-01 19:45:21 -0500

    contact icons

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -1,32 +1,48 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Input } from 'reactstrap';
+import Phone from '../images/phone.png';
+import Email from '../images/email.png';
+import GitHub from '../images/github.png';
+import Linkedin from '../images/linkedin.png';
+import Twitter from '../images/twitter.png';
 
 
 const Contact = () => {
     return(
         <div className="contact-page">
 
-        <h1>Contact</h1>
+        <h1>Let's talk</h1>
+        <div className="contact-content">
+        
+        <div className="form">
         <Form>
 
         <FormGroup>
-          <Label for="name">Name</Label>
-          <Input type="name" name="name" id="exampleName" placeholder="your name" />
+            <Input type="name" name="name" id="exampleName" placeholder="your name" />
         </FormGroup>
 
         <FormGroup>
-          <Label for="name">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="your email" />
+            <Input type="email" name="email" id="exampleEmail" placeholder="your email" />
         </FormGroup>
-
 
         <FormGroup>
-          <Label for="exampleMessage">Message</Label>
-          <Input type="message" name="message" id="message" />
+            <Input type="message" name="message" id="message" />
         </FormGroup>
 
-      </Form>
-        
+        </Form>
+        </div>
+
+        <div className="social-media">
+
+        <a className="sm-icon left linkedin"><img src={Linkedin} alt="linkedin icon"/></a>
+        <a className="sm-icon right twitter"><img src={Twitter} alt="twitter icon"/></a>
+        <a className="sm-icon left github"><img src={GitHub} alt="github icon"/></a>
+        <a className="sm-icon right email"><img src={Email} alt="email icon"/></a>
+        <a className="sm-icon left phone"><img src={Phone} alt="phone icon"/></a>
+
+        </div>
+        </div>
+
         </div>
     )
 }
