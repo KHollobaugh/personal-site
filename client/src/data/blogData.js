80dae0848e385b2e14cef0c9ee3971973a34ee47
KHollobaugh <khollobaugh@gmail.com>
2019-02-02 23:42:31 -0500

    added data

---
+++ client/src/data/blogData.js
@@ -0,0 +1,14 @@
+const blogData = [{
+    title: "Six Steps to Building your Website from Scratch",
+    subtitle: "So you figured out some HTML and CSS. What’s next?",
+    image: "https://cdn-images-1.medium.com/max/1600/1*BhtpKeZTdwLm73z-CEUEjw.jpeg",
+    url: "https://medium.com/@khollobaugh/https-medium-com-khollobaugh-six-steps-to-building-your-website-from-scratch-a713288cc6d"
+},
+{
+    title: "Helping ESL Students Succeed in the Remote Coding Classroom",
+    subtitle: "As remote tech education expands, so does our student base. How do we keep it accessible?",
+    image: "https://cdn-images-1.medium.com/max/1600/1*Nxpy6SPw-7V6uiYWp_GsQg.jpeg",
+    url: "https://medium.com/@khollobaugh/helping-esl-students-succeed-in-the-remote-coding-classroom-9f22b5b3050e"
+}]
+
+export default blogData;
---
+++ client/src/data/projectData.js
@@ -0,0 +1,39 @@
+const projectData = [{
+    title: "S&J Architecture",
+    date: "9.27.2018",
+    description: "Fully responsive static site",
+    technologies: "HTML, LESS, JavaScript",
+    snapshot: 'https://media.giphy.com/media/cI4ZhHLNQgbvCyVw2o/giphy.gif',
+    rendering: 'https://sj-architecture.netlify.com/',
+    code: 'https://github.com/KHollobaugh/User-Interface-Project-Week'
+},
+{
+    title: "To-Do App",
+    date: "10.04.2018",
+    description: "Simple To-do app with create, read, and delete",
+    technologies: "ReactJS",
+    snapshot: 'https://media.giphy.com/media/3oo7JlS3KxY1GW6Lq4/giphy.gif',
+    rendering: 'https://katys-lambda-list.netlify.com/',
+    code: 'https://github.com/KHollobaugh/React-Todo'
+},
+// {
+//     title: "Instagram Clone",
+//     date: "10.11.2018",
+//     description: "Clone of desktop instagram with log-in",
+//     technologies: "ReactJS, Styled Components",
+//     snapshot: 'https://media.giphy.com/media/80jPcMnuDA05OgWp7U/giphy.gif',
+//     rendering: 'https://elifturkphotography.netlify.com/',
+//     code: 'https://github.com/KHollobaugh/photography-website'
+// },
+{
+    title: "Elif Turk Photography",
+    date: "10.24.2018",
+    description: "Photography website for independent client",
+    technologies: "ReactJS",
+    snapshot: 'https://media.giphy.com/media/80jPcMnuDA05OgWp7U/giphy.gif',
+    rendering: 'https://elifturkphotography.netlify.com/',
+    code: 'https://github.com/KHollobaugh/photography-website'
+}
+]
+
+export default projectData;
