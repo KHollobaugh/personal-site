da1b01a5dadb1d07d597f8fb761d89771feb255c
KHollobaugh <khollobaugh@gmail.com>
2019-02-04 21:35:49 -0500

    mobile view html structure

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -13,16 +13,25 @@ const Home = () => {
 
         <h1>Hi, I'm Katy.</h1>
         <img src={ProfilePic} alt='profile'/>
-        <p className="desktop-view">
+        <div className="desktop-view">
+          <p>
         I'm a full-stack JavaScript developer with experience in state management systems like Redux and GraphQL, testing with Jest, and working with relational databases.<br/>
 
         After graduating university, I attended a start-up hackathon and became interested in pursuing tech. This led me to Lambda School, where I completed the Web Development curriculum and went on to work in a full-time role managing Teaching Assistants, while continuing side projects of my own.<br/>
 
         I love working closely with my team on a daily basis, and this role has helped me cultivated strong communication and leadership skills.
-        </p>
-        <p className="mobile-view">
-        Full-stack JavaScript Developer<br/>
-        </p>
+        </p>  
+        </div>
+        
+        <div className="mobile-view">
+            <h3>I'm a Full-stack JavaScript Developer</h3>
+            <p>
+        I like to build meaningful projects that help people,
+        learn new technologies,
+        and wake up early to drink strong coffee.
+            </p>  
+        </div>
+        
         <div className="tech-icons">
         <div className="line-one">
         <img src={Reactjs} alt="React.js" className="icon react"/>
