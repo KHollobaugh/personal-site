d163a13709353b8627d16a06ab0b0e583d8a346a
KHollobaugh <khollobaugh@gmail.com>
2019-02-04 21:28:40 -0500

    added tech icons

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ProfilePic from '../images/profile-pic.jpg';
+import GraphQL from '../images/GraphQL.png';
+import JavaScript from '../images/JavaScript.png';
+import Less from '../images/Less.png';
+import Reactjs from '../images/React2.png';
+import Redux from '../images/Redux.png';
+import SQL from '../images/SQL.png';
 
 const Home = () => {
     return(
@@ -7,9 +13,28 @@ const Home = () => {
 
         <h1>Hi, I'm Katy.</h1>
         <img src={ProfilePic} alt='profile'/>
-        <p>
-        That's what painting is all about. It should make you feel good when you paint. Learn when to stop. We have a fantastic little sky! We're trying to teach you a technique here and how to use it. There's nothing wrong with having a tree as a friend. See there, told you that would be easy.
+        <p className="desktop-view">
+        I'm a full-stack JavaScript developer with experience in state management systems like Redux and GraphQL, testing with Jest, and working with relational databases.<br/>
+
+        After graduating university, I attended a start-up hackathon and became interested in pursuing tech. This led me to Lambda School, where I completed the Web Development curriculum and went on to work in a full-time role managing Teaching Assistants, while continuing side projects of my own.<br/>
+
+        I love working closely with my team on a daily basis, and this role has helped me cultivated strong communication and leadership skills.
         </p>
+        <p className="mobile-view">
+        Full-stack JavaScript Developer<br/>
+        </p>
+        <div className="tech-icons">
+        <div className="line-one">
+        <img src={Reactjs} alt="React.js" className="icon react"/>
+        <img src={GraphQL} alt="GraphQL" className="icon graphql"/>
+        <img src={Redux} alt="Redux" className="icon redux"/>
+        </div>
+        <div className="line-two">
+        <img src={Less} alt="Less compiler" className="icon less"/>
+        <img src={SQL} alt="SQL database" className="icon sql"/>
+        <img src={JavaScript} alt="JavaScript" className="icon js"/>
+        </div>
+        </div>
 
         
         </div>
