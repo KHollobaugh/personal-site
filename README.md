aa5c5922a3402c0d4911197a770d29942f618dd2
KHollobaugh <khollobaugh@gmail.com>
2019-02-05 23:49:38 -0500

    readme and site code button

---
+++ README.md
@@ -0,0 +1,12 @@
+# Katy Hollobaugh's Portfolio Website
+
+## Intro
+This is a personal/professional portfolio with contact information, projects, and blog articles.
+
+Built with React
+
+## URL
+www.katyhollobaugh.com
+
+## Author
+**Katy Hollobaugh**
--- client/src/Pages/Portfolio.js
+++ client/src/Pages/Portfolio.js
@@ -22,6 +22,11 @@ class Portfolio extends Component {
     return (
         <div className="portfolio-page">
           <h1>Projects</h1>
+          <a href="https://github.com/KHollobaugh/personal-site"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="top-link">
+          View this site's code</a>
       <ProjectDisplay projects={this.state.projects}/>
         </div>
     )
