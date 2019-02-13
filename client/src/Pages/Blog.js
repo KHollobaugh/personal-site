c970416e3a847e029edca31adafe805af50d8c70
KHollobaugh <khollobaugh@gmail.com>
2019-02-12 21:56:58 -0500

    hooked up reading list page to blog

--- client/src/Pages/Blog.js
+++ client/src/Pages/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import blogData from '../data/blogData';
 import BlogDisplay from '../Components/BlogDisplay';
+import {NavLink} from 'react-router-dom';
 
 class Blog extends Component {
     constructor() {
@@ -23,6 +24,10 @@ class Blog extends Component {
         <div className="blog-page">
           <h1>Recent Articles</h1>
       <BlogDisplay articles={this.state.articles}/>
+        <NavLink className="reading-button" to={`/readinglist`}>
+          View My Reading List
+        </NavLink>
+
         </div>
     )
 }
